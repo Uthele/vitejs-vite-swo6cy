d0b54dbe7569689fe046ae91a0a2a0351d94fd7c
Uthele <98849628+Uthele@users.noreply.github.com>
2022-12-30 21:24:30 +0300

    1 file modified, 1 added

--- vite-React/public/app.js
+++ vite-React/public/app.js
@@ -1,3 +1,12 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Main from "./components/Main"
+
+export default function App() {
+  return (
+      <div className="container">
+          <Navbar />
+          <Main />
+      </div>
+  )
+}
